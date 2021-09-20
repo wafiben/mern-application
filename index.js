4c9825ed828db3d23bbaaea45863b065adb8eedf
Wafi benjeddou <wafibenjeddou@gmail.com>
2021-09-20 02:25:59 +0200

    user-post-update-put

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express=require('express');
 const mongoose=require('mongoose')
 const app=express()
 const dotenv=require('dotenv');
+const userRoute=require('./routes/userRoute');
 const PORT=process.env.PORT||8080
+app.use(express.json());
 dotenv.config();
-
+app.use('/',userRoute);
  mongoose
   .connect(process.env.MONGO, {
     useNewUrlParser: true,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,30 @@
 const mongoose=require('mongoose');
+const express=require('express')
+const User=require('../models/User')
 const router=express.Router();
+router.post('/createuser',(request,response)=>{
+    const {email,password}=request.body;
+    const newUser=new User({email,password}).save()
+    .then((data)=>{
+        console.log('this user was successfully saved ')
+        response.status(200)
+        response.json.send('user is registred '+data)
+    })
+    .catch((error)=>{
+        response.status(400)
+        response.send(error)
+    })
+})
+router.put('/createuser/:id',(request,response)=>{
+    User.findByIdAndUpdate(request.params.id,request.body,{new:true}).then((data)=>{
+        console.log('modifing user')
+        response.send('user is modified '+data)
+    })
+    .catch((error)=>{
+        console.log(error)
+        response.send(error)
+    })
+})
 
 
 
